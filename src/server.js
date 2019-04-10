073dccf196483794e62c765a207febbe143affcd
André Luiz Ximenes <andreluziximenes@gmail.com>
2019-04-09 22:46:41 -0300

    add variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ io.on('connection', socket => {
     })
 });
 
-//Middlware para criar o param io dentro de req, para ser acessivel em qualquer canto da aplicação.
+//Middleware para criar o param io dentro de req, para ser acessivel em qualquer canto da aplicação.
 app.use((req, res, next) => {
     req.io = io;
     return next();
@@ -36,4 +36,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use('/files/', express.static(path.resolve(__dirname, '..', 'tmp ')))
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
